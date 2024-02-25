b8c32ceed468c92ed182f9ee0a1c5afc9ae9a53c
dohun0310 <dohun0310@naver.com>
2024-02-25 14:13:08 +0900

    add MealContainer component

--- lib/components/container.dart
+++ lib/components/container.dart
@@ -13,6 +13,7 @@ class DateContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      alignment: Alignment.centerLeft,
       padding: const EdgeInsets.all(14),
       child: Text(
         date,
@@ -20,6 +21,68 @@ class DateContainer extends StatelessWidget {
           color: Theme.of(context).extension<AppExtension>()!.colors.text,
         ),
       )
+    );
+  }
+}
+
+class MealContainer extends StatelessWidget {
+  const MealContainer({
+    super.key,
+    required this.date,
+    required this.calorie,
+    required this.dish,
+  });
+
+  final String date;
+  final String calorie;
+  final String dish;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        DateContainer(date: date),
+        Container(
+          padding: const EdgeInsets.all(14),
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Theme.of(context).extension<AppExtension>()!.colors.outline,
+                width: 1
+              )
+            )
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "점심",
+                    style: ThemeTexts.subheadlineRegular.copyWith(
+                      color: Theme.of(context).extension<AppExtension>()!.colors.textSecondary,
+                    ),
+                  ),
+                  Text(
+                    calorie,
+                    style: ThemeTexts.subheadlineRegular.copyWith(
+                      color: Theme.of(context).extension<AppExtension>()!.colors.textSecondary,
+                    ),
+                  )
+                ]
+              ),
+              const SizedBox(height: 8),
+              Text(
+                dish,
+                style: ThemeTexts.subheadlineRegular.copyWith(
+                  color: Theme.of(context).extension<AppExtension>()!.colors.text,
+                ),
+              )
+            ],
+          )
+        ),
+      ]
     );
   }
 }
