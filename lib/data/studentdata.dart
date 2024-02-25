955fda991d6d16e64a758800002582276a260638
dohun0310 <dohun0310@naver.com>
2024-02-26 01:26:21 +0900

    edit StudentData logic

--- lib/data/studentdata.dart
+++ lib/data/studentdata.dart
@@ -1,9 +1,9 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-class Studentdata {
-  Studentdata._privateConstructor();
+class StudentData {
+  StudentData._privateConstructor();
 
-  static final Studentdata instance = Studentdata._privateConstructor();
+  static final StudentData instance = StudentData._privateConstructor();
 
   int? grade;
   int? classNumber;
@@ -12,11 +12,5 @@ class Studentdata {
     final prefs = await SharedPreferences.getInstance();
     grade = prefs.getInt("grade");
     classNumber = prefs.getInt("classNumber");
-  }
-
-  Future<void> saveStudentData() async {
-    final prefs = await SharedPreferences.getInstance();
-    if (grade != null) await prefs.setInt("grade", grade!);
-    if (classNumber != null) await prefs.setInt("classNumber", classNumber!);
   }
 }
