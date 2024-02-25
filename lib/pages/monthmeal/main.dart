c56b1e0fc721a3850f69f1b4381f5b7f6b79180c
dohun0310 <dohun0310@naver.com>
2024-02-25 14:43:05 +0900

    edit MonthMealPage

--- lib/pages/monthmeal/main.dart
+++ lib/pages/monthmeal/main.dart
@@ -18,41 +18,61 @@ class MonthMealPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              MealContainer(
-                date: "1월 1일 월요일",
-                calorie: "000.0kcal",
-                dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+              Column(
+                children: [
+                  DateContainer(date: "1월 1일 월요일"),
+                  MealContainer(
+                    calorie: "000.0kcal",
+                    dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+                  ),
+                ],
               ),
-              MealContainer(
-                date: "1월 2일 화요일",
-                calorie: "000.0kcal",
-                dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+              Column(
+                children: [
+                  DateContainer(date: "1월 2일 화요일"),
+                  MealContainer(
+                    calorie: "000.0kcal",
+                    dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+                  ),
+                ],
               ),
-              MealContainer(
-                date: "1월 3일 수요일",
-                calorie: "000.0kcal",
-                dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+              Column(
+                children: [
+                  DateContainer(date: "1월 3일 수요일"),
+                  MealContainer(
+                    calorie: "000.0kcal",
+                    dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+                  ),
+                ],
               ),
-              MealContainer(
-                date: "1월 4일 목요일",
-                calorie: "000.0kcal",
-                dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+              Column(
+                children: [
+                  DateContainer(date: "1월 4일 목요일"),
+                  MealContainer(
+                    calorie: "000.0kcal",
+                    dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+                  ),
+                ],
               ),
-              MealContainer(
-                date: "1월 5일 금요일",
-                calorie: "000.0kcal",
-                dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+              Column(
+                children: [
+                  DateContainer(date: "1월 5일 금요일"),
+                  MealContainer(
+                    calorie: "000.0kcal",
+                    dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+                  ),
+                ],
               ),
-              MealContainer(
-                date: "1월 7일 월요일",
-                calorie: "000.0kcal",
-                dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+              Column(
+                children: [
+                  DateContainer(date: "1월 7일 월요일"),
+                  MealContainer(
+                    calorie: "000.0kcal",
+                    dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
+                  ),
+                ],
               ),
-              MealContainer(
-                date: "1월 8일 화요일",
-                calorie: "000.0kcal",
-                dish: "셀프스펨무스비\n잔치국수\n<국수고명>매콤애호박채볶음\n돈육고구마강정\n김치무침\n스틱단무지\n<음료>얼박(시원한여름보내세요)"
-              ),
+
             ],
           )
         )
