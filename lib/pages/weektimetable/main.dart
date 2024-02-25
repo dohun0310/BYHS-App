2d857a5d697eb1e4132308a0cf7760d82fa5adef
dohun0310 <dohun0310@naver.com>
2024-02-25 14:43:25 +0900

    edit WeekTimeTablePage

--- lib/pages/weektimetable/main.dart
+++ lib/pages/weektimetable/main.dart
@@ -16,30 +16,41 @@ class WeekTimeTablePage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              TimeTableContainer(
-                date: "1월 1일 월요일",
-                period: ["1", "2", "3", "4", "5", "6"],
-                subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I"]
+              Column(
+                children: [
+                  DateContainer(date: "1월 1일 월요일"),
+                  TimeTableContainer(
+                    period: ["1", "2", "3", "4", "5", "6"],
+                    subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I"]
+                  ),
+                ],
               ),
-              TimeTableContainer(
-                date: "1월 2일 화요일",
-                period: ["1", "2", "3", "4", "5", "6", "7"],
-                subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I", "체육"]
+              Column(
+                children: [
+                  DateContainer(date: "1월 1일 월요일"),
+                  TimeTableContainer(
+                    period: ["1", "2", "3", "4", "5", "6"],
+                    subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I"]
+                  ),
+                ],
               ),
-              TimeTableContainer(
-                date: "1월 3일 수요일",
-                period: ["1", "2", "3", "4", "5", "6"],
-                subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I", "체육"]
+              Column(
+                children: [
+                  DateContainer(date: "1월 1일 월요일"),
+                  TimeTableContainer(
+                    period: ["1", "2", "3", "4", "5", "6"],
+                    subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I"]
+                  ),
+                ],
               ),
-              TimeTableContainer(
-                date: "1월 4일 목요일",
-                period: ["1", "2", "3", "4", "5", "6"],
-                subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I", "체육"]
-              ),
-              TimeTableContainer(
-                date: "1월 5일 금요일",
-                period: ["1", "2", "3", "4", "5", "6"],
-                subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I", "체육"]
+              Column(
+                children: [
+                  DateContainer(date: "1월 1일 월요일"),
+                  TimeTableContainer(
+                    period: ["1", "2", "3", "4", "5", "6"],
+                    subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I"]
+                  ),
+                ],
               ),
             ],
           )
