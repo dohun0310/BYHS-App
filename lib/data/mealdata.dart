665a4aa4a3d0ded3c28ef3e40eb9df7e4f846412
dohun0310 <dohun0310@naver.com>
2024-02-26 21:27:46 +0900

    edit errorList title

--- lib/data/mealdata.dart
+++ lib/data/mealdata.dart
@@ -19,7 +19,7 @@ List<Map<String, dynamic>> errorList = [
     "date": today,
     "details": {
       "calorie": "000.0 kcal",
-      "dish": "정보를 가져오지 못했어요.\n인터넷 연결 상태를 확인해보고 오류라고 생각된다면, 개발자에게 문의해주세요",
+      "dish": "정보를 가져오지 못했어요.\n인터넷 연결 상태에 문제가 없다면\n개발자에게 문의해주세요.",
     }
   }
 ];
