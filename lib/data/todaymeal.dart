b07e677f577645feccbb061e53784fd36787a401
dohun0310 <dohun0310@naver.com>
2024-02-26 20:58:12 +0900

    edit error handling in fetchTodayMeal

--- lib/data/todaymeal.dart
+++ lib/data/todaymeal.dart
@@ -2,6 +2,28 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'package:byhsapp/data/datedata.dart';
+
+List<Map<String, dynamic>> notFoundList = [
+  {
+    "date": today,
+    "details": {
+      "calorie": "000.0 kcal",
+      "dish": "정보가 존재하지 않아요.\n오류라고 생각된다면, 개발자에게 문의해주세요",
+    }
+  }
+];
+
+List<Map<String, dynamic>> errorList = [
+  {
+    "date": today,
+    "details": {
+      "calorie": "000.0 kcal",
+      "dish": "정보를 가져오지 못했어요.\n인터넷 연결 상태를 확인해보고 오류라고 생각된다면, 개발자에게 문의해주세요",
+    }
+  }
+];
+
 Future<List<Map<String, dynamic>>> fetchTodayMeal() async {
   final url = Uri.parse("${dotenv.get("API_URL")}/getTodayMeal");
   final response = await http.get(url);
@@ -22,7 +44,11 @@ Future<List<Map<String, dynamic>>> fetchTodayMeal() async {
     }
 
     return todayMeals;
+  } else if (response.statusCode == 404) {
+    return notFoundList;
+  } else if (response.statusCode == 500) {
+    return errorList;
   } else {
-    throw Exception("Failed to load month meal");
+    return errorList;
   }
 }
