b5ca6f3b0eab02934b6c431fcc032d942a34907f
dohun0310 <dohun0310@naver.com>
2024-02-27 00:43:49 +0900

    edit to use API data in weektimetable

--- lib/pages/weektimetable/main.dart
+++ lib/pages/weektimetable/main.dart
@@ -3,56 +3,65 @@ import 'package:flutter/material.dart';
 import 'package:byhsapp/components/appbar.dart';
 import 'package:byhsapp/components/container.dart';
 
-class WeekTimeTablePage extends StatelessWidget {
+import 'package:byhsapp/data/timetabledata.dart';
+
+class WeekTimeTablePage extends StatefulWidget {
   const WeekTimeTablePage({super.key});
 
   @override
+  WeekTimeTablePageState createState() => WeekTimeTablePageState();
+}
+
+class WeekTimeTablePageState extends State<WeekTimeTablePage> {
+  List<String> date = [];
+  List<List<String>> period = [];
+  List<List<String>> subject = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getWeekTimeTable();
+  }
+
+  void getWeekTimeTable() async {
+    final timeTable = TimeTable(dateRange: "Week");
+    List<Map<String, dynamic>> weekTimeTables = await timeTable.fetchTimeTable();
+
+    setState(() {
+      for (var item in weekTimeTables) {
+        date.add(item["date"]);
+        List<String> periods = [];
+        List<String> subjects = [];
+        for (var i in item["details"]["period"]) {
+          periods.add(i);
+        }
+        for (var i in item["details"]["subject"]) {
+          subjects.add(i);
+        }
+        period.add(periods);
+        subject.add(subjects);
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PageAppBar(
-        title: "시간표"
-      ),
+    return Scaffold(
+      appBar: const PageAppBar(title: "시간표"),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-            children: [
+            children: List.generate(date.length, (index) => 
               Column(
                 children: [
-                  DateContainer(date: "1월 1일 월요일"),
+                  DateContainer(date: date[index]),
                   TimeTableContainer(
-                    period: ["1", "2", "3", "4", "5", "6"],
-                    subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I"]
+                    period: period[index],
+                    subject: subject[index],
                   ),
-                ],
-              ),
-              Column(
-                children: [
-                  DateContainer(date: "1월 1일 월요일"),
-                  TimeTableContainer(
-                    period: ["1", "2", "3", "4", "5", "6"],
-                    subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I"]
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  DateContainer(date: "1월 1일 월요일"),
-                  TimeTableContainer(
-                    period: ["1", "2", "3", "4", "5", "6"],
-                    subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I"]
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  DateContainer(date: "1월 1일 월요일"),
-                  TimeTableContainer(
-                    period: ["1", "2", "3", "4", "5", "6"],
-                    subject: ["문학", "영어I", "수학I", "물리학I", "화학I", "지구과학I"]
-                  ),
-                ],
-              ),
-            ],
+                ]
+              )
+            )
           )
         )
       )
