91dd98252df06d0a9075d3648272ebfe0e80d845
dohun0310 <dohun0310@naver.com>
2024-02-27 16:52:35 +0900

    edit to use layout component in WeekTimaTablePage

--- lib/pages/weektimetable/main.dart
+++ lib/pages/weektimetable/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:byhsapp/components/appbar.dart';
-import 'package:byhsapp/components/container.dart';
+import 'package:byhsapp/components/layout.dart';
 
 import 'package:byhsapp/data/timetabledata.dart';
 
@@ -52,98 +51,18 @@ class WeekTimeTablePageState extends State<WeekTimeTablePage> {
     if (isTablet || isLandscape) {
       return TabletLayout(
         isTablet: isTablet,
+        type: "TimeTable",
         date: date,
         period: period,
         subject: subject,
       );
     } else {
       return MobileLayout(
+        type: "TimeTable",
         date: date,
         period: period,
         subject: subject,
       );
     }
-  }
-}
-
-class MobileLayout extends StatelessWidget {
-  const MobileLayout({
-    super.key,
-    required this.date,
-    required this.period,
-    required this.subject,
-  });
-
-  final List<String> date;
-  final List<List<String>> period;
-  final List<List<String>> subject;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const PageAppBar(title: "시간표"),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: List.generate(date.length, (index) => 
-              Column(
-                children: [
-                  DateContainer(date: date[index]),
-                  TimeTableContainer(
-                    period: period[index],
-                    subject: subject[index],
-                  ),
-                ]
-              )
-            )
-          )
-        )
-      )
-    );
-  }
-}
-
-class TabletLayout extends StatelessWidget {
-  const TabletLayout({
-    super.key,
-    required this.isTablet,
-    required this.date,
-    required this.period,
-    required this.subject,
-  });
-
-  final bool isTablet;
-  final List<String> date;
-  final List<List<String>> period;
-  final List<List<String>> subject;
-
-  @override
-  Widget build(BuildContext context) {
-    double realWidth = MediaQuery.of(context).size.width;
-    double safeAreaHorizontalPadding = MediaQuery.of(context).padding.left + MediaQuery.of(context).padding.right;
-    final screenWidth = realWidth - safeAreaHorizontalPadding;
-
-    return Scaffold(
-      appBar: const PageAppBar(title: "시간표"),
-      body: SafeArea(
-        child: GridView.count(
-          crossAxisCount: 2,
-          childAspectRatio: (screenWidth / 2) / 371,
-          children: List.generate(date.length, (index) => 
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                DateContainer(date: date[index]),
-                TimeTableContainer(
-                  period: period[index],
-                  subject: subject[index],
-                  height: 322,
-                ),
-              ]
-            )
-          )
-        )
-      )
-    );
   }
 }
