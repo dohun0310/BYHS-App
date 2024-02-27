26c67763b76902bb96a161f6b37b4bc8593ef311
dohun0310 <dohun0310@naver.com>
2024-02-27 15:51:53 +0900

    add TabletLayout in MonthMealPage

--- lib/pages/monthmeal/main.dart
+++ lib/pages/monthmeal/main.dart
@@ -38,6 +38,40 @@ class MonthMealPageState extends State<MonthMealPage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final isTablet = MediaQuery.of(context).size.shortestSide >= 600;
+
+    if (isTablet || isLandscape) {
+      return TabletLayout(
+        isTablet: isTablet,
+        date: date,
+        calorie: calorie,
+        dish: dish,
+      );
+    } else {
+      return MobileLayout(
+        date: date,
+        calorie: calorie,
+        dish: dish,
+      );
+    }
+  }
+}
+
+class MobileLayout extends StatelessWidget {
+  const MobileLayout({
+    super.key,
+    required this.date,
+    required this.calorie,
+    required this.dish,
+  });
+  
+  final List<String> date;
+  final List<String> calorie;
+  final List<String> dish;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: const PageAppBar(title: "급식"),
       body: SafeArea(
@@ -53,6 +87,50 @@ class MonthMealPageState extends State<MonthMealPage> {
                   ),
                 ]
               )
+            )
+          )
+        )
+      )
+    );
+  }
+}
+
+class TabletLayout extends StatelessWidget {
+  const TabletLayout({
+    super.key,
+    required this.isTablet,
+    required this.date,
+    required this.calorie,
+    required this.dish,
+  });
+
+  final bool isTablet;
+  final List<String> date;
+  final List<String> calorie;
+  final List<String> dish;
+
+  @override
+  Widget build(BuildContext context) {
+    double realWidth = MediaQuery.of(context).size.width;
+    double safeAreaHorizontalPadding = MediaQuery.of(context).padding.left + MediaQuery.of(context).padding.right;
+    final screenWidth = realWidth - safeAreaHorizontalPadding;
+
+    return Scaffold(
+      appBar: const PageAppBar(title: "급식"),
+      body: SafeArea(
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: (screenWidth / 2) / 271,
+          children: List.generate(date.length, (index) => 
+            Column(
+              children: [
+                DateContainer(date: date[index]),
+                MealContainer(
+                  calorie: calorie[index],
+                  dish: dish[index],
+                  height: 222,
+                ),
+              ]
             )
           )
         )
