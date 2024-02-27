6f13d92056ae52fc4fdbd680901b832856470ad8
dohun0310 <dohun0310@naver.com>
2024-02-27 15:46:05 +0900

    add TabletLayout in WeekTimeTable Page

--- lib/pages/weektimetable/main.dart
+++ lib/pages/weektimetable/main.dart
@@ -46,6 +46,40 @@ class WeekTimeTablePageState extends State<WeekTimeTablePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final isTablet = MediaQuery.of(context).size.shortestSide >= 600;
+
+    if (isTablet || isLandscape) {
+      return TabletLayout(
+        isTablet: isTablet,
+        date: date,
+        period: period,
+        subject: subject,
+      );
+    } else {
+      return MobileLayout(
+        date: date,
+        period: period,
+        subject: subject,
+      );
+    }
+  }
+}
+
+class MobileLayout extends StatelessWidget {
+  const MobileLayout({
+    super.key,
+    required this.date,
+    required this.period,
+    required this.subject,
+  });
+
+  final List<String> date;
+  final List<List<String>> period;
+  final List<List<String>> subject;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: const PageAppBar(title: "시간표"),
       body: SafeArea(
@@ -61,6 +95,51 @@ class WeekTimeTablePageState extends State<WeekTimeTablePage> {
                   ),
                 ]
               )
+            )
+          )
+        )
+      )
+    );
+  }
+}
+
+class TabletLayout extends StatelessWidget {
+  const TabletLayout({
+    super.key,
+    required this.isTablet,
+    required this.date,
+    required this.period,
+    required this.subject,
+  });
+
+  final bool isTablet;
+  final List<String> date;
+  final List<List<String>> period;
+  final List<List<String>> subject;
+
+  @override
+  Widget build(BuildContext context) {
+    double realWidth = MediaQuery.of(context).size.width;
+    double safeAreaHorizontalPadding = MediaQuery.of(context).padding.left + MediaQuery.of(context).padding.right;
+    final screenWidth = realWidth - safeAreaHorizontalPadding;
+
+    return Scaffold(
+      appBar: const PageAppBar(title: "시간표"),
+      body: SafeArea(
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: (screenWidth / 2) / 371,
+          children: List.generate(date.length, (index) => 
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                DateContainer(date: date[index]),
+                TimeTableContainer(
+                  period: period[index],
+                  subject: subject[index],
+                  height: 322,
+                ),
+              ]
             )
           )
         )
