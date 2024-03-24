2bd319fb16be2b6b68a6c95e24e23eca76651c74
dohun0310 <dohun0310@naver.com>
2024-03-24 14:37:02 +0900

    add send api_url to App Group logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_widget/home_widget.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -11,8 +12,12 @@ import 'package:byhsapp/pages/onboarding/main.dart';
 void main() async {
   await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
+
   final prefs = await SharedPreferences.getInstance();
   final onboardingComplete = prefs.getBool("onboardingComplete") ?? false;
+
+  await HomeWidget.setAppGroupId("group.studentinfo");
+  await HomeWidget.saveWidgetData<String>("api_url", dotenv.env["API_URL"]);
 
   runApp(MyApp(onboardingComplete: onboardingComplete));
 }
