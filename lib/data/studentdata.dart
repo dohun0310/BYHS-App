f9936886afa208e347b6ee0a3effc6c3087289ed
dohun0310 <dohun0310@naver.com>
2024-03-24 14:40:57 +0900

    add send studentinfo to App Group logic

--- lib/data/studentdata.dart
+++ lib/data/studentdata.dart
@@ -1,3 +1,4 @@
+import 'package:home_widget/home_widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class StudentData {
@@ -12,5 +13,13 @@ class StudentData {
     final prefs = await SharedPreferences.getInstance();
     grade = prefs.getInt("grade");
     classNumber = prefs.getInt("classNumber");
+
+    await HomeWidget.updateWidget(
+      name: "TimeTableProvider",
+      iOSName: "TimeTable",
+    );
+
+    await HomeWidget.saveWidgetData<int>("grade", grade ?? 1);
+    await HomeWidget.saveWidgetData<int>("classNumber", classNumber ?? 1);
   }
 }
